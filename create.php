4df355efd01ddfa5a0e306ba15f2f0ddfa69de65
icamanaksholeh <124222611+chamz-ae@users.noreply.github.com>
2024-07-17 11:24:45 +0700

    update

--- create.php
+++ create.php
@@ -45,6 +45,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>create-product</title>
+    <style>
+        body {
+    background: #44A08D;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #093637, #44A08D);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #093637, #44A08D);
+    }
+    .btn {
+    cursor: pointer;
+    position: relative;
+    padding: 10px 20px;
+    background: rgb(3, 247, 226);
+    font-size: 14px;
+    color: black;
+    border-top-right-radius: 7px;
+    border-bottom-left-radius: 7px;
+    transition: all 0.5s;
+    margin-bottom: 90px;
+}
+.btn:after, .btn:before {
+    content: " ";
+    width: 7px;
+    height: 7px;
+    position: absolute;
+    border: 0px solid #fff;
+    transition: all 1s;
+}
+.btn:after {
+    top: -1px;
+    left: -1px;
+    border-top: 3px solid black;
+    border-left: 3px solid black;
+}
+.btn:before {
+    bottom: -1px;
+    right: -1px;
+    border-bottom: 3px solid black;
+    border-right: 3px solid black;
+}
+.btn:hover {
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+}
+.btn:hover:before, .btn:hover:after {
+    width: 100%;
+    height: 100%;
+}
+.data-container {
+    background: #ffebee;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+    </style>
 </head>
 <body style="text-align: center; margin-top: 200px;">
 <h2>Create New Product</h2>
@@ -59,14 +113,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <input type="radio" id="Minuman" name="jenis" value="Minuman"> <label for="Minuman">Minuman</label><br>
 
-    <input type="submit" name="Create" value="Create">
+    <input class="btn" style="margin-bottom: 30px; margin-top: 20px;" type="submit" name="Create" value="Create">
     </form>
 
     <!-- <form action="show-product.php" method="GET">
       <input type="button" nama="show" href="show-product.php" value="SHOW" href="show-product.php">
     </form> -->
-    <div style="margin-top: 10px;">
-    <a style="text-decoration: none; color: blue;" href="show-product.php">Show Product</a>
+    <div style="margin-top: 10px; color: black;">
+    <a class="btn" style="text-decoration: none; color: blue;" href="show-product.php">Show Product</a>
     </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -53,6 +53,88 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LogIn</title>
     <link rel="stylesheet" href="styles.css">
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap');
+
+html, body {
+    height: 70%;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #44A08D;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #093637, #44A08D);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #093637, #44A08D);
+}
+
+.login-box {
+    position: relative;
+    width: 400px;
+    padding: 30px;
+    background: linear-gradient(to right, #093637, #44A08D);
+    box-sizing: border-box;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+    text-align: center;
+}
+
+.login-box h1 {
+    margin: 0 0 10px;
+    color: #fff;
+}
+
+.login-box .user-box {
+    position: relative;
+}
+
+.login-box .user-box input {
+    width: 100%;
+    padding: 25px 0;
+    font-size: 14px;
+    color: #fff;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+}
+
+.login-box .user-box label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: 0.5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+    top: -20px;
+    left: 0;
+    color: #03e9f4;
+    font-size: 12px;
+}
+
+.login-box form button {
+    display: inline-block;
+    padding: 10px 5px;
+    color: #03e9f4;
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: 0.5s;
+    margin-top: 10px;
+    letter-spacing: 5px;
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+    </style>
 </head>
 <body style="text-align: center; margin-top: 200px;">
 <main>
